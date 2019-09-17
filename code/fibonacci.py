cdc6b26e0b67429e950bd667edeb3604c7fc91a3
Abby <abigailcarey123@gmail.com>
2019-09-17 14:59:44 -0700

    Adding new function for fibonacci at an index

--- code/fibonacci.py
+++ code/fibonacci.py
@@ -8,3 +8,16 @@ def fibonacci_series(n):
         else:
             series.append(series[i - 1] + series[i - 2])
     return series
+
+def fibonacci_number_for_index(n):
+    fn_2 = 0
+    fn_1 = 1
+    number = 0
+    for i in range(0, n + 1):
+        if i <= 1:
+            number = i
+        else:
+            number = fn_1 + fn_2
+            fn_2 = fn_1
+            fn_1 = number
+    return number
